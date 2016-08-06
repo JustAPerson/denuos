13136319e52cd8384ebba656b3e459248c37e853
Jason Priest <jpriest@mit.edu>
2016-08-06 02:59:31 -0400

    Initial VGA buffer implementation
    Taken from Phillopp Oppermann's Rust OS blog series.
    Will be modifying soon.

--- src/kernel/lib.rs
+++ src/kernel/lib.rs
@@ -2,16 +2,13 @@
 #![no_std]
 
 extern crate rlibc;
+extern crate spin;
 
 mod vestige;
+#[macro_use] mod vga;
 
 #[no_mangle]
 pub extern fn kmain() {
-    use core::slice;
-
-    unsafe {
-        let vga = slice::from_raw_parts_mut(0xb8000 as *mut u16, 80 * 25);
-        vga[80] = (0x3f << 8) | 'H' as u16;
-        vga[81] = (0x3f << 8) | 'i' as u16;
-    }
+    vga::clear_screen();
+    println!("Hello, world!");
 }

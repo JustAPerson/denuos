a519e65e9b2054d16fd74b358f3bf79976c717ba
Jason Priest <jpriest@mit.edu>
2016-09-01 17:02:39 -0500

    Re-export architecture independent intrinsics

--- src/kernel/arch/mod.rs
+++ src/kernel/arch/mod.rs
@@ -1,2 +1,8 @@
 #[cfg(target_arch = "x86_64")]
 pub mod x86;
+
+/// Architecture independent intrinsics
+#[cfg(target_arch = "x86_64")]
+pub mod intrinsics {
+    pub use super::x86::intrinsics::halt;
+}
